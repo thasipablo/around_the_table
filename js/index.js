5d0de7f8f68ed8b117870dfb6b9730cf195e27d0
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-07-07 01:38:59 +0100

    add toogle mobile menu feature

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log('Welcome around the table...');
+const hamburgerMenuBtn = document.querySelector(".hamburger-menu");
+const closeMobileMenuBtn = document.querySelector(".close-menu");
+const mobileMenu = document.querySelector(".mobile-menu-wrapper");
+
+const hideElement = () => {
+  mobileMenu.classList.toggle('hidden');
+  console.log('clicked ....');
+};
+
+hamburgerMenuBtn.addEventListener('click', hideElement);
+closeMobileMenuBtn.addEventListener('click', hideElement);
